5e24bfb550784102b2efef55644caec53d24c841
Skovand <Skovand@proton.me>
2025-02-04 04:30:05 +0300

    indices

---
+++ indices/indices.py
@@ -0,0 +1,22 @@
+
+# A function to find all indices for a given char
+
+
+
+def all_index(seq: str | list, char: str):
+
+    """ Function to return all indices
+    of a specified character
+
+    :param seq: A sequence from which the indices are needed
+    :type seq: str, list or tuple
+
+    :param char: A char(letter) occupying the indices
+    :type char: str
+
+    :return: a list of indices
+    :rtype: list
+
+    """
+
+    ...
