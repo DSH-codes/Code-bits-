3e22761f151c8ace81414530b10165d4423e5f79
Skovand <Skovand@proton.me>
2025-02-05 19:58:47 +0300

    Function to find all indices

--- indices/indices.py
+++ indices/indices.py
@@ -4,35 +4,50 @@
 
 
 def all_index(seq: str | list, char: str):
-
-    """ Function to return all indices
+    """
+    Function to return all indices
     of a specified character
 
-    :param seq: A sequence from which the indices are needed
-    :type seq: str, list or tuple
+    Parameters:
+        seq (str, list): a string/list to search
+        character occurrences in
+        char (str): a character indices of which
+        to find
 
-    :param char: A char(letter) occupying the indices
-    :type char: str
+    Returns:
+        list: a list of indices
 
-    :return: a list of indices
-    :rtype: list
+    Raises:
+        TypeError: if seq is not a str/list, or
+        if seq is a list, but one of the items
+        is not a string type
 
+    Examples:
+        >>> all_index("hello world", "o")
+        [4, 7]
+        >>> all_index(["1", "2", "3", "2"], "2")
+        [1, 3]
+        >>> all_index("axaxa", "a")
+        [0, 2, 4]
+        >>> all_index("hello world", "x")
+        []
     """
 
     seq = "".join(seq) if type(seq) == list else seq        # convert the sequence into a string, if it is a list
     indices = []                                            # a list to collect indices
-    twin = seq                                              # a copy to perform editing on
 
-    for i in twin:
+    for i in seq:
 
-        if i == char:
+        if i == char:                                       # if i is the char we search
 
-            indices.append(seq.index(i))
-            seq = seq.replace(i, "-", 1)
+            indices.append(seq.index(i))                    # add its index to the list
+            seq = seq.replace(i, "-", 1)                    # then replace its first occurrence, by a dash, to prevent getting the same index again
 
     return indices
 
 
-s = all_index(list("I am writing a code"), "a")
+if __name__ == "__main__":
 
-print(s)
+    s = all_index("hello world", "o")
+    print(s)
+
